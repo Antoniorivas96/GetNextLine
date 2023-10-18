dfcb9f86a8c4a63a95a5079df625f2f763ccf902
anrivas- <127803128+Antoniorivas96@users.noreply.github.com>
2023-10-18 16:15:11 +0200

    Add files via upload

---
+++ dd/get_next_line.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anrivas- <anrivas-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 16:40:39 by anrivas-          #+#    #+#             */
+/*   Updated: 2023/07/19 13:15:45 by anrivas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*s2;
+
+	len = ft_strlen (s1) + 1;
+	s2 = malloc (len);
+	if (!s2)
+		return (0);
+	ft_memcpy (s2, s1, len);
+	return ((char *) s2);
+}
+
+static char	*copyfile_to_backup(int fd, char *buf, char *backup)
+{
+	int		read_line;
+	char	*char_temp;
+
+	read_line = 1;
+	while (read_line != '\0')
+	{
+		read_line = read(fd, buf, BUFFER_SIZE);
+		if (read_line == -1)
+			return (NULL);
+		else if (read_line == 0)
+			break ;
+		buf[read_line] = '\0';
+		if (!backup)
+			backup = ft_strdup("");
+		char_temp = backup;
+		backup = ft_strjoin(char_temp, buf);
+		free(char_temp);
+		char_temp = NULL;
+		if (ft_strchr (buf, '\n') && ft_strchr (buf, '\0'))
+			break ;
+		else if (ft_strchr (buf, '\n'))
+			break ;
+	}
+	return (backup);
+}
+
+static char	*extract_substr_after_nl(char *line)
+{
+	size_t	count;
+	char	*backup;
+
+	count = 0;
+	while (line[count] != '\n' && line[count] != '\0')
+		count++;
+	if (count == ft_strlen(line) - 1)
+		return (NULL);
+	if (line[count] == '\0' || line[1] == '\0')
+		return (0);
+	backup = ft_substr(line, count + 1, ft_strlen(line) - count);
+	if (backup[0] == '\0')
+	{
+		free(backup);
+		backup = NULL;
+	}
+	line[count + 1] = '\0';
+	return (backup);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buf;
+	static char	*backup;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (0);
+	line = copyfile_to_backup(fd, buf, backup);
+	free(buf);
+	if (!line)
+	{
+		free(backup);
+		backup = NULL;
+		return (NULL);
+	}
+	backup = extract_substr_after_nl(line);
+	return (line);
+}
+
+/*
+int	main(void)
+{
+	int		fd;
+
+	fd = open("read_error.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	close(fd);
+	return (0);
+}
+*/
---
+++ dd/get_next_line_utils.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anrivas- <anrivas-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 16:40:48 by anrivas-          #+#    #+#             */
+/*   Updated: 2023/07/18 13:16:37 by anrivas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str3;
+	size_t	tam;
+
+	if (!s1 || !s2)
+		return (0);
+	tam = ft_strlen(s1) + ft_strlen(s2);
+	str3 = (char *)malloc (tam + 1);
+	if (!str3)
+		return (0);
+	ft_memcpy(str3, s1, ft_strlen(s1));
+	ft_memcpy(str3 + ft_strlen(s1), s2, ft_strlen(s2) + 1);
+	return (str3);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	cont;
+
+	if (!src && !dst)
+		return (0);
+	cont = 0;
+	while (cont < n)
+	{
+		*((unsigned char *)(dst + cont)) = *((unsigned char *)(src + cont));
+		cont++;
+	}
+	return (dst);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	cont;
+
+	cont = 0;
+	while (s[cont])
+		cont++;
+	return (cont);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	if ((!*str && (char)c == '\0'))
+		return ((char *)str);
+	while (*str != '\0')
+	{
+		if (*str == (char) c)
+			return ((char *) str);
+		str++;
+		if (*str == (char) c)
+			return ((char *) str);
+	}
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	tam;
+
+	tam = ft_strlen(s);
+	if (tam <= start)
+		return (0);
+	if (tam - start < len)
+		len = tam - start;
+	substr = (char *)malloc (len + 1);
+	if (!substr)
+		return (0);
+	ft_memcpy(substr, s + start, len);
+	substr[len] = '\0';
+	return (substr);
+}
